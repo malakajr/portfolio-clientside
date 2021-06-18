a3185d7532c7d7a22a2cb3ef575d0bb05a053d01
malakajr <61264864+malakajr@users.noreply.github.com>
2021-06-18 16:26:40 +0530

    commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/*Hamburger Menu Modifications*/
+const bodyElement=document.querySelector('body');
+const menuToggle=document.querySelector('.hamburger-menu');
+
+menuToggle.addEventListener('click', function () {
+    bodyElement.classList.toggle('is-open');
+});
+
+
